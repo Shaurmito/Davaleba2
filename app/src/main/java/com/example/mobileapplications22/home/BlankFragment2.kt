2fef27e297a41c010faf8dee90e8d3d74c2ba322
Shaurmito <sandrolocho@gmail.com>
2024-12-27 23:52:41 +0400

    commit message

---
+++ app/src/main/java/com/example/mobileapplications22/home/BlankFragment2.kt
@@ -0,0 +1,14 @@
+package com.example.mobileapplications22.home
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mobileapplications22.R
+
+
+class BlankFragment2() : Fragment(R.layout.fragment_blank4){
+}
